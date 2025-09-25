4b48ad312569cf011c60b039de84420a762d30ed
Vanessa Phipps <vanessa.phipps@cfa.harvard.edu>
2025-09-25 13:23:00 -0400

    Intro: background is black before image load

--- src/components/SpeclabExtrasIntro.vue
+++ src/components/SpeclabExtrasIntro.vue
@@ -1,16 +1,20 @@
 <template>
-  <div class="speclab-extras-intro-header-image text-light d-flex flex-column justify-content-end">
-    <BContainer>
-      <BRow>
-        <BCol cols="12" xl="10" xxl="8">
-          <p class="mb-5">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras et pulvinar lectus, eu
-            sodales leo. Nullam eleifend bibendum sem quis euismod. Etiam a efficitur neque, id
-            pretium augue. Pellentesque egestas finibus tellus.
-          </p>
-        </BCol>
-      </BRow>
-    </BContainer>
+  <div class="bg-gen-black">
+    <div
+      class="speclab-extras-intro-header-image text-light d-flex flex-column justify-content-end"
+    >
+      <BContainer>
+        <BRow>
+          <BCol cols="12" xl="10" xxl="8">
+            <p class="mb-5">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras et pulvinar lectus, eu
+              sodales leo. Nullam eleifend bibendum sem quis euismod. Etiam a efficitur neque, id
+              pretium augue. Pellentesque egestas finibus tellus.
+            </p>
+          </BCol>
+        </BRow>
+      </BContainer>
+    </div>
   </div>
 </template>
 
